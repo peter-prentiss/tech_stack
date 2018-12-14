ee02849be132bf56ace623a5b87c01f69e8b73f4
Peter <peter.prentiss@gmail.com>
2018-12-14 08:05:47 -0600

    librarylist returns libraries

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 
 class LibraryList extends Component {
     render() {
+        console.log(this.props);
         return (
 
         );
@@ -10,7 +11,7 @@ class LibraryList extends Component {
 }
 
 const mapStateToProps = state => {
-    console.log(state);
+    return { libraries: state.libraries };
 };
 
 export default connect(mapStateToProps)(LibraryList);
