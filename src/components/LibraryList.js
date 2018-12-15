2a647e5022515d16fd90908078d5bdfc433ca474
Peter <peter.prentiss@gmail.com>
2018-12-15 15:38:09 -0600

    Add flatlist to librarylist

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
 
 class LibraryList extends Component {
-    render() {
-        console.log(this.props);
-        return (
+    renderItem(library) {
 
+    }
+
+    render() {
+        return (
+            <FlatList 
+                data={this.props.libraries}
+                renderItem={this.renderItem}
+                keyExtractor={library => library.id}
+            />
         );
     }
 }
