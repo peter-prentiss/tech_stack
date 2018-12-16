cb51bc5e087175df66d13e447dbec8442e1727bd
Peter <peter.prentiss@gmail.com>
2018-12-16 14:16:14 -0600

    Bugs!

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -4,23 +4,21 @@ import { connect } from 'react-redux';
 import ListItem from './ListItem';
 
 class LibraryList extends Component {
-    renderItem(library) {
-        return <ListItem library={library} />
+    renderItem({ item }) {
+        return <ListItem library={item} />;
     }
 
     render() {
         return (
             <FlatList 
-                data={this.props.libraries}
-                renderItem={this.renderItem}
-                keyExtractor={library => library.id}
+                data = { this.props.libraries }
+                renderItem = { this.renderItem }
+                keyExtractor = {(item) => item.id.toString()}
             />
-        );
-    }
+     );
+  }
 }
-
 const mapStateToProps = state => {
     return { libraries: state.libraries };
 };
-
 export default connect(mapStateToProps)(LibraryList);
